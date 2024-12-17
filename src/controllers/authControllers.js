c648249463993d1c8d42b1133434bd767da042ce
shreyash010 <shreyashwaghmare0501@gmail.com>
2024-12-17 21:54:24 +0530

    fix: `user model import fix`

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const Organization = require('../models/Organization');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const Constants = require('../utils/constants');
 const bcrypt = require('bcryptjs');
 
